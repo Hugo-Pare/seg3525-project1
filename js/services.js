f1e1567cdc22417de1d4f6b660ea9f75213b0408
Hugo Paré <71098494+Hugo-Pare@users.noreply.github.com>
2022-06-06 10:46:54 -0400

    completed services page

---
+++ js/services.js
@@ -0,0 +1,37 @@
+function reservationDog(){
+    window.location.href='../pages/reservation-dog.html';
+};
+
+function reservationCat(){
+    window.location.href='../pages/reservation-cat.html';
+};
+
+function reservationBunny(){
+    window.location.href='../pages/reservation-bunny.html';
+};
+
+function changeDogColor(){
+    document.getElementById('dog').style.backgroundColor = '#a6a6a6';
+}
+
+function changeCatColor(){
+    document.getElementById('cat').style.backgroundColor = '#a6a6a6';
+}
+
+function changeBunnyColor(){
+    document.getElementById('bunny').style.backgroundColor = '#a6a6a6';
+}
+
+function resetColor(){
+    document.getElementById('dog').style.backgroundColor = '#cccccc';
+    document.getElementById('cat').style.backgroundColor = '#cccccc';
+    document.getElementById('bunny').style.backgroundColor = '#cccccc';
+}
+
+function changeReservationColor(){
+    document.getElementById('reservation-button').style.backgroundColor = '#a6a6a6';
+}
+
+function resetReservationColor(){
+    document.getElementById('reservation-button').style.backgroundColor = '#cccccc';
+}
